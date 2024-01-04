4a8bc4972693c434473af597391fb4f8228de19a
Dmitry Novikov <dredfort.42@gmail.com>
2024-01-04 12:16:58 +0000

    iOS app start point

---
+++ app/iOS/rupl/rupl Watch App/ruplApp.swift
@@ -0,0 +1,17 @@
+//
+//  ruplApp.swift
+//  rupl Watch App
+//
+//  Created by Dmitry Novikov on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct rupl_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ app/iOS/rupl/rupl/ruplApp.swift
@@ -0,0 +1,17 @@
+//
+//  ruplApp.swift
+//  rupl
+//
+//  Created by Dmitry Novikov on 04/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct ruplApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
