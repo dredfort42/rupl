4f9db2c0637c0a52b01e395dfb6b39266bcb9c27
dredfort42 <dredfort.42@gmail.com>
2024-03-13 11:31:26 +0000

    dashboard works

--- profile/internal/api/user_profile.go
+++ profile/internal/api/user_profile.go
@@ -1,19 +1,16 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
 
 	// "auth/internal/db"
 
-	"github.com/dgrijalva/jwt-go"
 	"github.com/dredfort42/tools/logprinter"
 	"github.com/gin-gonic/gin"
 )
 
 // Get user profile
 func GetUserProfile(c *gin.Context) {
-
 	var accessToken string
 	var errorResponse ResponseError
 	var err error
@@ -25,34 +22,22 @@ func GetUserProfile(c *gin.Context) {
 		return
 	}
 
-	authServerURL := fmt.Sprintf("http://%s:%s/api/v1/auth/verify", config["auth.host"], config["auth.port"])
-	client := &http.Client{}
+	if !ValidateAccessToken(accessToken) {
+		errorResponse.Error = "invalid_request"
+		errorResponse.ErrorDescription = "User not authenticated"
+		c.IndentedJSON(http.StatusUnauthorized, errorResponse)
+		return
+	}
 
-	req, err := http.NewRequest("GET", authServerURL, nil)
+	email, err := ParseToken(accessToken)
 	if err != nil {
-		fmt.Println("Error creating request:", err)
+		errorResponse.Error = "token_error"
+		errorResponse.ErrorDescription = "Failed to parse access token"
+		c.IndentedJSON(http.StatusUnauthorized, errorResponse)
 		return
 	}
 
-	req.Header.Set("Cookie", fmt.Sprintf("access_token=%s", accessToken))
-
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode == http.StatusOK {
-		logprinter.PrintSuccess("User authenticated: ", accessToken)
-	} else {
-		logprinter.PrintError("User not authenticated", nil)
-		return
-	}
-
-	userID, err := ParseToken(accessToken)
-
-	logprinter.PrintSuccess("User ID: ", userID)
+	logprinter.PrintSuccess("User ID: ", email)
 
 	// access_token, ok := c.")
 
@@ -71,20 +56,5 @@ func GetUserProfile(c *gin.Context) {
 	// 	return
 	// }
 
-	c.IndentedJSON(http.StatusOK, gin.H{"message": "User profile"})
-}
-
-// ParseToken parses token
-func ParseToken(tokenString string) (string, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return []byte("secret"), nil
-	})
-	if err != nil {
-		return "", err
-	}
-
-	claims := token.Claims.(jwt.MapClaims)
-	userID := claims["user_id"].(string)
-
-	return userID, nil
+	c.IndentedJSON(http.StatusOK, gin.H{"message": email})
 }
