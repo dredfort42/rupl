b496bcb1156530d6f51d6617f0f8569e6a5e3873
Dmitry Novikov <dredfort.42@gmail.com>
2024-03-26 22:43:40 +0000

    device auth compleate

--- auth/internal/api/device_deauthorization.go
+++ auth/internal/api/device_deauthorization.go
@@ -10,10 +10,8 @@ import (
 
 func DeviceDeauthorization(c *gin.Context) {
 	var clientID string
-	var tClientID string
 	var accessToken string
 	var errorResponse ResponseError
-	var err error
 
 	clientID = c.Request.URL.Query().Get("client_id")
 	accessToken = c.Request.URL.Query().Get("access_token")
@@ -22,33 +20,6 @@ func DeviceDeauthorization(c *gin.Context) {
 		errorResponse.Error = "invalid_request"
 		errorResponse.ErrorDescription = "Missing required parameter"
 		c.IndentedJSON(http.StatusBadRequest, errorResponse)
-		return
-	}
-
-	if tClientID, err = ParseToken(accessToken); err != nil {
-		errorResponse.Error = "token_error"
-		errorResponse.ErrorDescription = "Failed to parse access token"
-		c.IndentedJSON(http.StatusUnauthorized, errorResponse)
-		return
-	}
-
-	if clientID != tClientID {
-		errorResponse.Error = "token_error"
-		errorResponse.ErrorDescription = "Invalid access token"
-		c.IndentedJSON(http.StatusUnauthorized, errorResponse)
-		return
-	}
-
-	tokenHasExpired, err := TokenHasExpired(accessToken)
-	if err != nil {
-		errorResponse.Error = "token_error"
-		errorResponse.ErrorDescription = "Failed to check access token"
-		c.IndentedJSON(http.StatusUnauthorized, errorResponse)
-		return
-	} else if tokenHasExpired {
-		errorResponse.Error = "token_error"
-		errorResponse.ErrorDescription = "Access token has expired"
-		c.IndentedJSON(http.StatusUnauthorized, errorResponse)
 		return
 	}
 
