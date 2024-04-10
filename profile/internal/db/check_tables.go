f5ebb4d1eb7ee8e06cddf59863843077e96eddb7
Dmitry Novikov <dredfort.42@gmail.com>
2024-04-10 12:13:41 +0100

    check tabels logic changed

--- profile/internal/db/check_tables.go
+++ profile/internal/db/check_tables.go
@@ -6,16 +6,26 @@ import (
 	"github.com/dredfort42/tools/logprinter"
 )
 
+// CheckTableExists checks if the table exists
+func CheckTableExists(tabelName string) bool {
+	tabelExists := false
+
+	err := db.database.QueryRow("SELECT EXISTS (SELECT 1 FROM information_schema.tables WHERE table_name = $1)", tabelName).Scan(&tabelExists)
+	if err != nil || !tabelExists {
+		logprinter.PrintWarning("Table does not exist", tabelName)
+		return false
+	} else {
+		logprinter.PrintSuccess("Table found successfully", tabelName)
+		return true
+	}
+}
+
 // CheckProfilesTable checks if the users table exists, if not, it creates it
 func CheckProfilesTable() {
-	tabalExists := false
+	var tabalExists bool = CheckTableExists(db.tableProfiles)
 
 	for !tabalExists {
-		query := "SELECT * FROM " + db.tableProfiles + ";"
-
-		if _, db.err = db.database.Query(query); db.err != nil {
-			logprinter.PrintWarning("Table does not exist", db.tableProfiles)
-			query = `
+		query := `
 				CREATE TABLE IF NOT EXISTS ` + db.tableProfiles + ` (
 					email VARCHAR(255) PRIMARY KEY,
 					first_name VARCHAR(255) NOT NULL,
@@ -26,30 +36,22 @@ func CheckProfilesTable() {
 					updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 				);
 			`
-			if _, db.err = db.database.Exec(query); db.err != nil {
-				logprinter.PrintError("Failed to create table", db.err)
-			} else {
-				tabalExists = true
-				logprinter.PrintSuccess("Table successfully created", db.tableProfiles)
-			}
+		if _, db.err = db.database.Exec(query); db.err != nil {
+			logprinter.PrintError("Failed to create table", db.err)
+			time.Sleep(5 * time.Second)
 		} else {
 			tabalExists = true
-			logprinter.PrintSuccess("Table found successfully", db.tableProfiles)
+			logprinter.PrintSuccess("Table successfully created", db.tableProfiles)
 		}
-		time.Sleep(5 * time.Second)
 	}
 }
 
 // CheckDevicesTable checks if the devices table exists, if not, it creates it
 func CheckDevicesTable() {
-	tabalExists := false
+	var tabalExists bool = CheckTableExists(db.tableDevices)
 
 	for !tabalExists {
-		query := "SELECT * FROM " + db.tableDevices + ";"
-
-		if _, db.err = db.database.Query(query); db.err != nil {
-			logprinter.PrintWarning("Table does not exist", db.tableDevices)
-			query = `
+		query := `
 				CREATE TABLE IF NOT EXISTS ` + db.tableDevices + ` (
 					id SERIAL PRIMARY KEY,
 					email VARCHAR(255) NOT NULL,
@@ -63,17 +65,13 @@ func CheckDevicesTable() {
 					updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 				);
 			`
-			if _, db.err = db.database.Exec(query); db.err != nil {
-				logprinter.PrintError("Failed to create table", db.err)
-			} else {
-				tabalExists = true
-				logprinter.PrintSuccess("Table successfully created", db.tableDevices)
-			}
+		if _, db.err = db.database.Exec(query); db.err != nil {
+			logprinter.PrintError("Failed to create table", db.err)
+			time.Sleep(5 * time.Second)
 		} else {
 			tabalExists = true
-			logprinter.PrintSuccess("Table found successfully", db.tableDevices)
+			logprinter.PrintSuccess("Table successfully created", db.tableDevices)
 		}
-		time.Sleep(5 * time.Second)
 	}
 }
 
